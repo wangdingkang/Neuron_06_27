9a8a9a4824888ed3d2e2c568458df030ed0f9ae0
wangdingkang <1257332448@qq.com>
2017-06-28 15:11:14 -0400

    update dataset & py

--- plotter/plotter.py
+++ plotter/plotter.py
@@ -2,8 +2,9 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-input = 'data/test.swc'
-
+input1 = 'data/Giorgio-Re/PFC/TDI21301017_cell2_pia_spines.MA1.ASC.swc'
+input2 = 'data/Xiaojun-Re/L2/PFC/data_02.swc'
+output = 'image/neuron_02.eps'
 
 class Node:
 
@@ -20,38 +21,43 @@ class Node:
 
 if __name__ == '__main__':
 
-    nodes = []
+    fig = plt.figure(figsize=(20, 10))
+    ax1 = fig.add_subplot(121, projection='3d')
+    ax2 = fig.add_subplot(122, projection='3d')
 
-    with open(input, 'r') as file:
-        for line in file:
-            if line[0] != '#':
-                nodes.append(Node(line))
+    axs = [ax1, ax2]
+    files = [input1, input2]
+    for file, ax in zip(files, axs):
+        nodes = []
+        with open(file, 'r') as file:
+            for line in file:
+                if line[0] != '#':
+                    nodes.append(Node(line))
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+        axons_x, axons_y, axons_z, axons_r = [], [], [], []
+        somas_x, somas_y, somas_z, somas_r = [], [], [], []
+        dends_x, dends_y, dends_z, dends_r = [], [], [], []
 
-    axons_x, axons_y, axons_z, axons_r = [], [], [], []
-    somas_x, somas_y, somas_z, somas_r = [], [], [], []
-    dends_x, dends_y, dends_z, dends_r = [], [], [], []
+        for node in nodes:
+            if node.type == 1:
+                somas_x.append(node.X)
+                somas_y.append(node.Y)
+                somas_z.append(node.Z)
+                somas_r.append(node.radius)
+            elif node.type == 2:
+                axons_x.append(node.X)
+                axons_y.append(node.Y)
+                axons_z.append(node.Z)
+                axons_r.append(node.radius)
+            else: # == 3
+                dends_x.append(node.X)
+                dends_y.append(node.Y)
+                dends_z.append(node.Z)
+                dends_r.append(node.radius)
 
-    for node in nodes:
-        if node.type == 1:
-            somas_x.append(node.X)
-            somas_y.append(node.Y)
-            somas_z.append(node.Z)
-            somas_r.append(node.radius)
-        elif node.type == 2:
-            axons_x.append(node.X)
-            axons_y.append(node.Y)
-            axons_z.append(node.Z)
-            axons_r.append(node.radius)
-        else: # == 3
-            dends_x.append(node.X)
-            dends_y.append(node.Y)
-            dends_z.append(node.Z)
-            dends_r.append(node.radius)
+        ax.scatter(somas_x, somas_y, somas_z, color='g', s=somas_r)
+        ax.scatter(axons_x, axons_y, axons_z, color='r', s=axons_r)
+        ax.scatter(dends_x, dends_y, dends_z, color='b', s=dends_r)
 
-    ax.scatter(somas_x, somas_y, somas_z, color='g', s=somas_r)
-    ax.scatter(axons_x, axons_y, axons_z, color='r', s=axons_r)
-    ax.scatter(dends_x, dends_y, dends_z, color='b', s=dends_r)
-    plt.show()
+    fig.savefig(output, format='eps', dpi=1000)
+    plt.close(fig)
